3d83d297c659d50b783618ae50b5b96ad401d4c2
Carlos Alcala <alcala21@gmail.com>
2021-11-14 14:46:16 -0600

    Stage 3/6. Regex Engine.

--- regex.py
+++ regex.py
@@ -1,10 +1,17 @@
 # write your code here
-def regexp(exp, text):
+def match(exp, text):
     if not exp:
         return True
     if not text or (exp[0] != text[0] and exp[0] != "."):
         return False
-    return regexp(exp[1:], text[1:])
+    return match(exp[1:], text[1:])
 
+
+def regexp(pattern, string):
+    if match(pattern, string):
+        return True
+    if not string:
+        return False
+    return regexp(pattern, string[1:])
 
 print(regexp(*input().split("|")))
